235517fc836e0587990386db3cf26b8e893a5fe0
Eoghan Maxwell <eoghan.maxwell@kiwi.com>
2019-11-21 17:03:06 +0100

    adding apollo wrapper to app. Adding prettier

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from 'next/app'
+import { withApollo } from '../lib/apollo'
+
+class MyApp extends App<any> {
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
+}
+
+export default withApollo(MyApp)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import Link from "next/link"
+import gql from "graphql-tag";
+import Layout from "../components/Layout";
+import { NextPage } from "next"
 
 const IndexPage: NextPage = () => {
   return (
@@ -12,6 +14,24 @@ const IndexPage: NextPage = () => {
           <a>About</a>
         </Link>
       </p>
+      <Mutation
+        mutation={gql`
+          mutation{
+            login(password: "password", email: "test@test.com") {
+              id
+              firstName
+              lastName
+              email
+              name
+            }
+          }
+        `}
+      >
+        {(mutate: () => any) => (<button onClick={async () => {
+          const response = await mutate();
+          console.log({ response });
+        }}>Login</button>)}
+      </Mutation>
     </Layout>
   )
 }
